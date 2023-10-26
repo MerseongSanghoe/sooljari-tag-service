f1910ee05af6515db9c70e267bb008aa2c1e53de
Merseong <esc990720@korea.ac.kr>
2023-10-26 19:29:32 +0900

    [FEAT] morgan, neo4j-driver 추가, #2

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import neo4j from 'neo4j-driver';
 
 dotenv.config();
+
+let driver;
+try {
+    const [neoid, neopw] = process.env.NEO4J_AUTH.split('/');
+    driver = neo4j.driver(
+        process.env.NEO4J_URI,
+        neo4j.auth.basic(neoid, neopw)
+    );
+    const serverInfo = await driver.getServerInfo()
+    console.log('Connection established')
+    console.log(serverInfo)
+  } catch(err) {
+    console.log(`Connection error\n${err}\nCause: ${err.cause}`);
+		process.exit(1);
+  }
+
 
 const app = express();
 const port = process.env.SERVER_PORT
 
+if (process.env.NODE_ENV === 'production') { 
+   app.use(morgan('combined'));
+} else {
+   app.use(morgan('dev'));
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port} with ${process.env.NODE_ENV}`)
 })
+
+await driver.close();
