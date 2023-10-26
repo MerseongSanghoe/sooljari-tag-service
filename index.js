c3c8596fd2e97e73e721a1cc2e423ad509394495
Merseong <esc990720@korea.ac.kr>
2023-10-26 20:04:55 +0900

    [STYLE] prettier, eslint 적용

--- index.js
+++ index.js
@@ -7,35 +7,36 @@ dotenv.config();
 
 let driver;
 try {
-    const [neoid, neopw] = process.env.NEO4J_AUTH.split('/');
-    driver = neo4j.driver(
-        process.env.NEO4J_URI,
-        neo4j.auth.basic(neoid, neopw)
-    );
-    const serverInfo = await driver.getServerInfo()
-    console.log('Connection established')
-    console.log(serverInfo)
-  } catch(err) {
-    console.log(`Connection error\n${err}\nCause: ${err.cause}`);
-		process.exit(1);
-  }
-
+  const [neoid, neopw] = process.env.NEO4J_AUTH.split('/');
+  driver = neo4j.driver(
+    process.env.DATABASE_URI,
+    neo4j.auth.basic(neoid, neopw)
+  );
+  const serverInfo = await driver.getServerInfo();
+  console.log('Connection established');
+  console.log(serverInfo);
+} catch (err) {
+  console.log(`Connection error\n${err}\nCause: ${err.cause}`);
+  process.exit(1);
+}
 
 const app = express();
-const port = process.env.SERVER_PORT
+const port = process.env.SERVER_PORT;
 
-if (process.env.NODE_ENV === 'production') { 
-   app.use(morgan('combined'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
 } else {
-   app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port} with ${process.env.NODE_ENV}`)
-})
+  console.log(
+    `Example app listening on port ${port} with ${process.env.NODE_ENV}`
+  );
+});
 
 await driver.close();
